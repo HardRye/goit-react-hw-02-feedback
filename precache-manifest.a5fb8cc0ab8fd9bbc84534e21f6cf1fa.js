0aabf5e56f3bcaae09537be781b2616692fdbc5c
HardRain <nikolaimihail@gmail.com>
2020-10-10 01:11:05 +0300

    Updates

---
+++ precache-manifest.a5fb8cc0ab8fd9bbc84534e21f6cf1fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a8f8da8b006dcdb1099c5ada6adf6d",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "11960b8026ac0afafb56",
+    "url": "/goit-react-hw-02-feedback/static/css/main.daac1b38.chunk.css"
+  },
+  {
+    "revision": "1b1f2df1bf566fdb00df",
+    "url": "/goit-react-hw-02-feedback/static/js/2.45117538.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.45117538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11960b8026ac0afafb56",
+    "url": "/goit-react-hw-02-feedback/static/js/main.1343cc21.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
